7ba5cb3cb6d03d4b0c74e9c2e0c2fa61ee0f9e8d
deeptiKalasapura <kalasapura@gmail.com>
2018-05-09 09:29:34 +0530

    dark theme

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,14 @@
 from flask import render_template
+from sqlalchemy import func
 
 from . import home
+from ..models import Book, User
 
 
 @home.route('/')
 def homepage():
-    return render_template('home/index.html', title="Welcome")
+    users = User.query.all()
+    books = Book.query.all()
+    borrowed_books = Book.query.filter(Book.is_borrowed == 1).all()
+    return render_template('home/index.html', title="Welcome", users=len(users), books=len(books),
+                           borrowed_books=len(borrowed_books))
